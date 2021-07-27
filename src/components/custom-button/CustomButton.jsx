6c1f8b6ae22f5a0b2507ae7244f6dda82ad19821
Mbarachi <39647480+Mbarachi@users.noreply.github.com>
2021-07-27 10:51:20 +0100

    Added react-icons to the list of dependencies and created a new component search-bar

--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './custom-button.styles.css'
 import {Button} from 'react-bootstrap'
+import { BsPlus } from "react-icons/bs";
 
  const CustomButton = () => {
     return (
         <Button variant="primary" className="custom-btn">
-            Add New
+            <b>Add a new task</b> <BsPlus size="25"/>
         </Button>
     )
 }
